6326f98ff875eaa25cba222adddc0391e824e498
Angelos Zaimis <angeloszaimis@MacBook-Pro-von-Angelos.local>
2026-02-14 14:29:32 +0100

    feat: first working version

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,24 +1,24 @@
-CREATE TABLE TRANSACTION (
+CREATE TABLE transactions (
     id UUID PRIMARY KEY,
     external_transaction_id VARCHAR(255) NOT NULL,
-    customer_id UUID NOT NULL,
+    customer_id VARCHAR(64) NOT NULL,
     customer_age INT NOT NULL,
     amount NUMERIC(19, 2) NOT NULL,
     currency VARCHAR(3) NOT NULL,
-    country VARCHAR(255) NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-)
+    country VARCHAR(2) NOT NULL,
+    status VARCHAR(16) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
 
-create index idx_transaction_customer_id on TRANSACTION (customer_id);
-create index idx_transaction_created_at on TRANSACTION (created_at);
+create index idx_transaction_customer_id on transactions (customer_id);
+create index idx_transaction_created_at on transactions (created_at);
 
-CREATE TABLE RISK_ASSESSMENTS (
+CREATE TABLE risk_assessments (
     id UUID PRIMARY KEY,
-    transaction_id UUID NOT NULL UNIQUE REFERENCES TRANSACTION(id),
+    transaction_id UUID NOT NULL UNIQUE REFERENCES transactions(id),
     score INT NOT NULL,
     level VARCHAR(16) NOT NULL,
     reasons TEXT NOT NULL,
     assessed_at TIMESTAMP NOT NULL
-)
+);
 
